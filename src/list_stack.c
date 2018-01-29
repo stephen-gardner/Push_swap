ca5d231e4ef275382d5f935488d05c6bbc5adedc
Stephen Gardner <stephenbgardner@gmail.com>
2018-01-29 02:06:01 -0800

    Remove push_num; fix remove_num to change stack head appropriately

--- src/list_stack.c
+++ src/list_stack.c
@@ -6,7 +6,7 @@
 /*   By: sgardner <stephenbgardner@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/26 15:46:54 by sgardner          #+#    #+#             */
-/*   Updated: 2018/01/28 21:09:01 by sgardner         ###   ########.fr       */
+/*   Updated: 2018/01/29 01:48:28 by sgardner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,21 +39,16 @@ t_num	*create_num(int n)
 	return (num);
 }
 
-void	push_num(t_stack *stack, t_num *num)
-{
-	append_num(stack, num);
-	stack->head = num;
-}
-
 void	remove_num(t_stack *stack, t_num *num)
 {
-	if (stack->head == num)
+	if (stack->head == num && num->next == num)
 		stack->head = NULL;
 	else
 	{
 		num->prev->next = num->next;
 		num->next->prev = num->prev;
-		stack->head = num->next;
+		if (stack->head == num)
+			stack->head = num->next;
 	}
 	stack->size--;
 	num->prev = num;
